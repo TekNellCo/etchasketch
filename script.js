8ec08d216ec7e348091b2c93dcf9d8f6052bb064
NellyNell <129366078+TekNellCo@users.noreply.github.com>
2023-07-07 01:00:42 -0400

    fix problem with double colors/page loads on open/range bar works

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
 
 
-
 function populateBoard(size){
 
     let color = 'black';
     let gridSize = size * size;
     let grid = document.querySelector('.grid');
 
-grid.style.gridTemplateColumns = `repeat(${size} ,1fr)`;
-grid.style.gridTemplateRows = `repeat(${size} ,1fr)`;
+    let remove = grid.querySelectorAll('div');
+    remove.forEach((div)=> div.remove())
 
-for(let i = 0; i< gridSize; i++){
-    let square = document.createElement('div');
-    square.style.backgroundColor = 'grey';
-    grid.insertAdjacentElement('beforeend',square);
-    square.addEventListener('mouseover', ()=>{
+    grid.style.gridTemplateColumns = `repeat(${size} ,1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size} ,1fr)`;
+
+    for(let i = 0; i< gridSize; i++){
+        let square = document.createElement('div');
+        square.style.backgroundColor = 'grey';
+        grid.insertAdjacentElement('beforeend',square);
+        square.addEventListener('mouseover', ()=>{
         square.style.backgroundColor = color;
 
 
-        let reset = document.querySelector('.reset');
-        reset.addEventListener('click',()=>{
-            
-        
-            square.style.backgroundColor = 'grey';
-        });
+    let reset = document.querySelector('.reset');
+            reset.addEventListener('click',()=>{     
+            square.style.backgroundColor = 'grey';});
 
-        let blue = document.querySelector('.blue');
+    let blue = document.querySelector('.blue');
             blue.addEventListener('click',()=>{
                 return  color = 'blue';});
 
-        let black = document.querySelector('.black');
+    let black = document.querySelector('.black');
             black.addEventListener('click',()=>{
                 return  color = 'black';});
 
-        let orange = document.querySelector('.orange');
+    let orange = document.querySelector('.orange');
             orange.addEventListener('click',()=>{
                 return  color = 'orange';});
-        let random = document.querySelector('.random');
+    let random = document.querySelector('.random');
             random.addEventListener('click',()=>{
                 let r = Math.random() * 256;
                 let g = Math.random() * 256;
                 let b = Math.random() * 256;
-                     return color = `rgb(${r},${g},${b})`; });
+                return color = `rgb(${r},${g},${b})`; });
 
         
         
